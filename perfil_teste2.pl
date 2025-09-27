e0c9e0dd0fdfb40804e314c25ef2e6a77eca9374
haskaz313 <166724372+haskaz313@users.noreply.github.com>
2025-09-27 16:23:13 -0300

    Add files via upload
    estes sao os arquivos teste

---
+++ perfil_teste2.pl
@@ -0,0 +1,5 @@
+:- module(perfil_teste2,[perfil_teste2/2]).
+
+perfil_teste2(2, [big_data-n,hadoop-n,estatistica-n,redes-s,criptografia-s,firewall-s,
+          interface-n,sites-n,banco_de_dados-n,machine_learning-n,
+          redes_neurais-n,linguagem_natural-n,lideranca-n,governanca-n,gestao_projetos-n]).
---
+++ perfil_teste3.pl
@@ -0,0 +1,5 @@
+:- module(perfil_teste3,[perfil_teste3/2]).
+
+perfil_teste3(3, [big_data-n,hadoop-n,estatistica-n,redes-n,criptografia-n,firewall-n,
+          interface-s,sites-s,banco_de_dados-s,machine_learning-n,
+          redes_neurais-n,linguagem_natural-n,lideranca-s,governanca-n,gestao_projetos-n]).
