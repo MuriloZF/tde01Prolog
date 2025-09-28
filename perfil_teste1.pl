845de20381620b696b8b7ca2a43e3d8f4bb4c1b2
Murilo Zimerman Fortaleza <mzimermanfortaleza@gmail.com>
2025-09-28 13:30:10 -0300

    Changed the 'import' way to accommodate multiple files

--- perfil_teste1.pl
+++ perfil_teste1.pl
@@ -1,5 +1,5 @@
-:- module(perfil_teste1, [perfil_teste1/2]).
+:- multifile perfil_teste/2.
 
-perfil_teste1(1, [big_data-s,hadoop-n,estatistica-s,redes-n,criptografia-n,firewall-n,
+perfil_teste(1, [big_data-s,hadoop-n,estatistica-s,redes-n,criptografia-n,firewall-n,
           interface-n,sites-n,banco_de_dados-n,machine_learning-s,
           redes_neurais-s,linguagem_natural-n,lideranca-n,governanca-n,gestao_projetos-n]).
